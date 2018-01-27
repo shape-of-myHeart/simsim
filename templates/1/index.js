90b0786ded4a19a391643484853b2a4ad63b64fe
shape-of-myHeart <dragonrise0001@gmail.com>
2018-01-28 02:21:30 +0900

    Add files via upload

---
+++ templates/1/index.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+    const main = $('.main');
+
+    const sections = [
+        {
+            thumbnail: 'https://static.giantbomb.com/uploads/square_small/15/154387/2340397-dishonored_corvobase_v2.png',
+            caption: {
+                text: '이벤트',
+            }
+        },
+        {
+            thumbnail: 'https://s-media-cache-ak0.pinimg.com/originals/c0/5d/58/c05d58bc81dc1a3d280538ad6b162242.png',
+            caption: {
+                text: '이벤트',
+                backgroundColor: '#0080FF'
+            }
+        }
+    ];
+    const makeSection = ({ thumbnail, caption }) => `
+<div class="section" style="top: 0; right: 0;">
+    <div class="thumbnail" style="background-image: url(${thumbnail});"></div>
+    <div class="caption" style="background-color: ${caption.backgroundColor}">${caption.text}</div>
+</div>
+`;
+
+    for(const i in sections){
+        const section = sections[i];
+        main.append(makeSection(section));
+        // 테스트
+        main.append(makeSection(section));
+        main.append(makeSection(section));
+        main.append(makeSection(section));
+        main.append(makeSection(section));
+        main.append(makeSection(section));
+    }
+    
+});
